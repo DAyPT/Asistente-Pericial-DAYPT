bc9a128c7bd7d979cdf571a71b081bd7efecd9f6
Jesifu <151977074+Jesifu@users.noreply.github.com>
2026-03-07 13:30:10 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dapt-v2'; 
+const CACHE = 'dapt-v3'; 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(c => c.addAll([
@@ -14,5 +14,6 @@ self.addEventListener('install', e => {
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
+
 
 
